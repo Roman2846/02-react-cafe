5b7accb35f3f99a6bdf52d3d11490635d3360f48
Roman <krasivskyi01@gmail.com>
2026-07-05 20:21:50 +0300

    Move App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import CafeInfo from "./components/CafeInfo/CafeInfo";
-import VoteOptions from "./components/VoteOptions/VoteOptions";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 import css from "./App.module.css";
-import type { VoteType, Votes } from "./types/votes";
-import VoteStats from "./components/VoteStats/VoteStats";
-import Notification from "./components/Notification/Notification";
+import type { VoteType, Votes } from "../../types/votes";
 
 function App() {
   const [votes, setVotes] = useState<Votes>({
